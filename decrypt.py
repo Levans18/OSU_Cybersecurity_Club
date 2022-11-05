edeade8534eecdc6225f7c04760ebbd152aa2a90
Liam Evans <liamevans1818@gmail.com>
2022-11-04 21:42:32 -0400

    please

--- decrypt.py
+++ decrypt.py
@@ -1,10 +1,9 @@
 from Crypto.Cipher import AES
 
-plaintext = ""
 
 key = b"Secret_16ByteKey"
 cipher = AES.new(key, AES.MODE_ECB)
 
-ciphertext = cipher.encrypt(plaintext)
-print(ciphertext)
 # Output: b'\x0e\xd9?Z\xdf\xa5\x11,]\xa3\\ax\xe3\x19\xe0'
+decrypted = cipher.decrypt(b'\x0e\xd9?Z\xdf\xa5\x11,]\xa3\\ax\xe3\x19\xe0')
+print(decrypted)
